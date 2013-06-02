c553290a0924f2f8499203cfa7fc037cda56d77f
Adam Alinauskas <aalinauskas@gmail.com>
2013-06-02 16:41:06 -0600

    You can move the bucket around on the screen with the touchpad or mouse.Its very nice on the mac with the three finger touch.

--- drop/src/com/me/drop/Drop.java
+++ drop/src/com/me/drop/Drop.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Drop implements ApplicationListener {
 
@@ -18,6 +20,11 @@ public class Drop implements ApplicationListener {
 	Texture bucketImage;
 	Sound dropSound;
 	Music rainMusic;
+	
+	OrthographicCamera camera;
+	SpriteBatch batch;
+	
+	Rectangle bucket;
 	
 	@Override
 	public void create() {	
@@ -32,6 +39,17 @@ public class Drop implements ApplicationListener {
 		//start the playback of the background music immediately
 		rainMusic.setLooping(true);
 		rainMusic.play();
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,800,480);
+		
+		batch = new SpriteBatch();
+		
+		bucket = new Rectangle();
+		bucket.x = 800/2- 48/2;
+		bucket.y= 20;
+		bucket.width = 48;
+		bucket.height = 48;		
 	}
 
 	/* (non-Javadoc)
@@ -43,8 +61,24 @@ public class Drop implements ApplicationListener {
 
 	@Override
 	public void render() {		
-//		Gdx.gl.glClearColor(1, 1, 1, 1);
-//		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage,bucket.x,bucket.y);
+		batch.end();
+		
+		if(Gdx.input.isTouched()){
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(),Gdx.input.getY(),0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 48 /2;
+			
+		}
+		
 	}
 
 	@Override
